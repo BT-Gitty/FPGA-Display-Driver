c9286415278e61b06b89edd68e2ddd8be2fac4bb
Brian <biranpt@gmail.com>
2024-06-21 19:53:05 +0100

    Add files via upload

---
+++ sim/pxl_clock_tb.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 21.06.2024 19:46:24
+// Design Name: 
+// Module Name: pxl_clock_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module pxl_clock_tb(
+
+    );
+endmodule
---
+++ sources/pxl_clock.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 21.06.2024 19:45:32
+// Design Name: 
+// Module Name: pxl_clock
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module pxl_clock(
+
+    );
+endmodule
